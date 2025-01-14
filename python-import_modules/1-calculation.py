d755a33872c63abfb4f347bf66f2e4c4192269ec
mpesaran <mahsa.pesaran@gmail.com>
2025-01-14 14:51:06 +1100

    import functions separetly

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from calculator_1.py import *
+from calculator_1.py import add, sub, mul, div
 
 if __name__ == "__main__":
     a = 10
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} // {} = {}".format(a, b, div(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
