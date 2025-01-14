5398a850218efa052bce4f345e1423429634c082
mpesaran <mahsa.pesaran@gmail.com>
2025-01-14 15:16:48 +1100

    Dlete 2 extra charactersin print

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,8 +3,11 @@ if __name__ == "__main__":
     import sys
     argv = sys.argv[1:]
     argc = len(argv)
-    print("{} arguments:".format(argc))
+    if argc == 1:
+        print("{} argument:".format(argc))
+    else:
+        print("{} arguments:".format(argc))
     i = 1
     for arg in argv:
-        print("{} : {}".format(i, arg))
+        print("{}: {}".format(i, arg))
         i += 1
