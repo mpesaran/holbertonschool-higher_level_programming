8ae25ece7fdfe89fad07033495f416cbdabcaec8
mpesaran <mahsa.pesaran@gmail.com>
2025-01-20 08:40:34 +1100

    Add a function to replace an index in a list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if 0<idx<len(my_list):
+    if 0<=idx<=len(my_list):
         return my_list[idx]
     return
+
+my_list = [1, 2, 3, 4, 5]
+idx = 0
+print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
