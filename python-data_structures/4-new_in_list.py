b9725456b9fb71e2197b91c39146dc3e729faa4f
mpesaran <mahsa.pesaran@gmail.com>
2025-01-20 12:55:46 +1100

    Delte test lines

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -7,12 +7,3 @@ def new_in_list(my_list, idx, element):
         new_list = new_list+my_list[idx+1:]
         return new_list
     return my_list
-
-
-my_list = [1, 2, 3]
-idx = 3
-new_element = 4
-new_list = new_in_list(my_list, idx, new_element)
-
-print(new_list)
-print(my_list)
