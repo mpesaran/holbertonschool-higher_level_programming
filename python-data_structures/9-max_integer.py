af6d251fb480032b469316a706d155120ff7878f
mpesaran <mahsa.pesaran@gmail.com>
2025-01-20 13:02:29 +1100

    Change initial value to first item in list

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = 0
+    max = my_list[0]
     if len(my_list) == 0:
         return None
-    for i in range(len(my_list)):
+    for i in range(1, len(my_list)):
         if my_list[i] > max:
             max = my_list[i]
     return max
