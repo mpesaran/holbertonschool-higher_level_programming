c113381cb89ec7e9183819020ffa80ca91eaa2c0
mpesaran <mahsa.pesaran@gmail.com>
2025-01-20 12:41:01 +1100

    Use reversed method for itereating inreverse order

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     length = len(my_list)
-    for i in range(0, length):
-        print("{:d}".format(length - i))
+    for i in reversed(range(0, length)):
+        print("{:d}".format(my_list[i]))
+
+my_list = [1, 2, "H", 9]
+print_reversed_list_integer(my_list)
