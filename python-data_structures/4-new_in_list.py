bec0e3e57016afc0fe9244b0e4fb0b0d1f40a49c
mpesaran <mahsa.pesaran@gmail.com>
2025-01-20 11:42:09 +1100

    Edit style for all the files

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = []
-    if 0<=idx<=len(my_list):
+    if 0 <= idx <= len(my_list):
         new_list = my_list[:idx]
         new_list.append(element)
         new_list = new_list+my_list[idx+1:]
--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -5,7 +5,3 @@ def multiple_returns(sentence):
     else:
         t = (len(sentence), sentence[0])
     return t
-
-sentence = ""
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
