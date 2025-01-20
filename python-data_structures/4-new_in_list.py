385ca326bb80bf6e2b9df43083ff48b8125c760d
mpesaran <mahsa.pesaran@gmail.com>
2025-01-20 12:55:06 +1100

    Return input list if index not in range

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -6,4 +6,13 @@ def new_in_list(my_list, idx, element):
         new_list.append(element)
         new_list = new_list+my_list[idx+1:]
         return new_list
-    return
+    return my_list
+
+
+my_list = [1, 2, 3]
+idx = 3
+new_element = 4
+new_list = new_in_list(my_list, idx, new_element)
+
+print(new_list)
+print(my_list)
