c4e819340f2bbfffae9f86790df45a78ecd1e3cd
mpesaran <mahsa.pesaran@gmail.com>
2025-01-20 12:30:51 +1100

    Add decimal format to integer print formats

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     length = len(my_list)
     for i in range(0, length):
-        print("{}".format(length - i))
+        print("{:d}".format(length - i))
