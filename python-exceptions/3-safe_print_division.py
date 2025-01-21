d44afbd1529f70af811453e4be2b6c628d42d6ae
mpesaran <mahsa.pesaran@gmail.com>
2025-01-21 15:25:52 +1100

    Add an if statement inside finally

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    result = 0
+    result = None
     try:
         result = a // b
-        return result
+        return float(result)
     except ZeroDivisionError:
         return "None"
     finally:
-        print("Inside result: {:d}".format(result))
-
-# a = 12
-# b = 2
-# result = safe_print_division(a, b)
-# print("{:d} / {:d} = {}".format(a, b, result))
-
-# a = 12
-# b = 0
-# result = safe_print_division(a, b)
-# print("{:d} / {:d} = {}".format(a, b, result))
+        if result is None:
+            print("Inside result: {}".format("None"))
+        else:
+            print("Inside result: {:0.1f}".format(result))
