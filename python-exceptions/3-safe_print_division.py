fb4e2fcaa28928f723577537e9fb65c6fc6b939c
mpesaran <mahsa.pesaran@gmail.com>
2025-01-22 08:16:18 +1100

    Add a function to raise a type error

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,12 +2,23 @@
 def safe_print_division(a, b):
     result = None
     try:
-        result = a // b
+        result = a / b
         return float(result)
     except ZeroDivisionError:
-        return "None"
+        result = None
     finally:
         if result is None:
-            print("Inside result: {}".format("None"))
+            print("Inside result: {}".format(None))
         else:
             print("Inside result: {:0.1f}".format(result))
+
+
+a = 11
+b = 2
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
+
+a = 12
+b = 0
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
---
+++ python-exceptions/5-raise_exception.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def raise_exception():
+    raise TypeError("There is a Type Error")
