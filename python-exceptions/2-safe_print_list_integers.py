3426f5f0fbb270ae4ab5271f0af46b3b4528b650
mpesaran <mahsa.pesaran@gmail.com>
2025-01-21 15:16:53 +1100

    Add a functoion to divide two integers

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,7 @@
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     num = 0
-    while count!= x:
+    while count != x:
         try:
             print("{:d}".format(my_list[count]), end="")
             num += 1
