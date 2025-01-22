d0656e0446989af9ca435b807bca641df6e7ec16
mpesaran <mahsa.pesaran@gmail.com>
2025-01-22 11:30:41 +1100

    Try to fail checker

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary:
-        return max(a_dictionary, key=a_dictionary.get)
-    return "None"
+    # if (a_dictionary):
+    #     max_value = max(a_dictionary)
+    #     #else:
+    #     return (max_value)
+    #     # return (max(a_dictionary))
+    # else:
+    #     return (None)
