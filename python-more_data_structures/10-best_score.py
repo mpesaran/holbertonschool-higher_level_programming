3c370f6f6c65d49dd25fb073ba421c73579b5d69
mpesaran <mahsa.pesaran@gmail.com>
2025-01-22 11:33:53 +1100

    Correct the code like before

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    # if (a_dictionary):
-    #     max_value = max(a_dictionary)
-    #     #else:
-    #     return (max_value)
-    #     # return (max(a_dictionary))
-    # else:
-    #     return (None)
+    if a_dictionary:
+        return max(a_dictionary)
+    return None
