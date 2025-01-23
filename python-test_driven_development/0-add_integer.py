6f1fa63a4fe48f6f6af4089be554f608d7ec3e6c
mpesaran <mahsa.pesaran@gmail.com>
2025-01-24 09:26:27 +1100

    Add documentation,delete try/except

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    ''' This is to test'''
-    try:
-        if a is None:
-            raise ValueError
-        if not isinstance(b, int):
-            raise TypeError
-        if isinstance(a, float) or isinstance(a, float):
-            a = int(a)
-            b = int(b)
-        return a + b
-    except TypeError:
-        print("b must be an integer")
-    except ValueError:
-        print("a must be an integer")
+    ''' Adds two integers or floats, casting them to integers if necessary.
+    Raises:
+        TypeError: If a or b is not an integer or float.
+    '''
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b
+
