b27f376157001dae3532c4a8cec2783d4d1c4498
mpesaran <mahsa.pesaran@gmail.com>
2025-01-23 13:54:35 +1100

    Add a test text file to test function

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    try:
+        if a is None:
+            raise ValueError
+        if not isinstance(b, int):
+            raise TypeError
+        if isinstance(a, float) or isinstance(a, float):
+            a = int(a)
+            b = int(b)
+        return  a + b
+    except TypeError:
+        print("b must be an integer")
+    except ValueError:
+        print("a must be an integer")
+
+
+print(add_integer("e",2.2))
+
