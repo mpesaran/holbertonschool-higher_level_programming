1d4a09375c4d9007be74a09f523c84a1bcf94832
mpesaran <mahsa.pesaran@gmail.com>
2025-01-24 12:37:44 +1100

    change line break to pass the tests

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -13,11 +13,11 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
     if not isinstance(matrix, list) \
             or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix \
-                        (list of lists) of integers/floats")
+        raise TypeError\
+            ("matrix must be a matrix (list of lists) of integers/floats")
     if not all(isinstance(el, (int, float)) for row in matrix for el in row):
-        raise TypeError("matrix must be a matrix \
-                        (list of lists) of integers/floats")
+        raise TypeError\
+            ("matrix must be a matrix (list of lists) of integers/floats")
     row_length = len(matrix[0])
     if not all(len(row) == row_length for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
