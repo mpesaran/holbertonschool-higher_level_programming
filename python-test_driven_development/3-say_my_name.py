8caf34c7ed9e8fc096d5ecae5aa0e1bbb207bf84
mpesaran <mahsa.pesaran@gmail.com>
2025-01-24 13:19:13 +1100

    Add an if statement for the none firat name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -6,6 +6,8 @@ prints first name and last name
 
 def say_my_name(first_name, last_name=""):
     ''' This function is to print out first name and last name'''
+    if first_name is None:
+        raise TypeError("first_name must be a string")
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
