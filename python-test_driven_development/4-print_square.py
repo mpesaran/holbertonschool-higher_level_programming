41e5d3962ff5c6fdb5daa72e593421e5b19e1a3c
mpesaran <mahsa.pesaran@gmail.com>
2025-01-24 14:26:37 +1100

    Add a function to print square of #

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+prints first name and last name
+"""
+
+
+def print_square(size):
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
