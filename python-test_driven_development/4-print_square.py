c1e73a720963eca57c4e711856f8dae913942998
mpesaran <mahsa.pesaran@gmail.com>
2025-01-24 14:32:11 +1100

    Add a condition for no argumets enterd

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -7,7 +7,7 @@ prints a square of # in size of size
 def print_square(size):
     """ Prints a square of '#' characters with the given size."""
     if size is None:
-        raise TypeError("size must be an integer")
+        raise TypeError("enter a value for size")
     if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
     if not isinstance(size, int):
