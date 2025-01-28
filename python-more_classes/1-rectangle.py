ec7cf9be992a83adc143ddeafea585ac3a0ac424
mpesaran <mahsa.pesaran@gmail.com>
2025-01-29 10:21:13 +1100

    Add methods and attributes inside class

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""This is a rectangle class to define a rectangle"""
+
+
+class Rectangle:
+    """initiates objects and set the height and width"""
+
+    def __init__(self, width=0, height=0):
+        """initiate the object with the values given"""
+        self.__width = width
+        self.__height = height
+    
+    @property
+    def width(self):
+        return self.__width
+    
+    @property
+    def height(self):
+        return self.__height
+    
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+    
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("hegiht must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+
+my_rectangle = Rectangle(2, 4)
+print(my_rectangle.__dict__)
