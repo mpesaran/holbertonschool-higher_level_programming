91564fa919c32afd35ea0ba67d8ad5bea3b004a5
mpesaran <mahsa.pesaran@gmail.com>
2025-01-29 10:43:17 +1100

    Add checking the value inside init function

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -7,21 +7,25 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """initiate the object with the values given"""
-        # if not isinstance(width, int):
-        #     raise TypeError("width must be an integer")
-        # if width < 0:
-        #     raise ValueError("width must be >= 0")
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
         self.__width = width
+        if not isinstance(height, int):
+            raise TypeError("hegiht must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__height = height
-    
+
     @property
     def width(self):
         return self.__width
-    
+
     @property
     def height(self):
         return self.__height
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -29,7 +33,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
