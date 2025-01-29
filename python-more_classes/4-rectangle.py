a3ac579fc2da1da699c1c2899b3f0477d6feef4b
mpesaran <mahsa.pesaran@gmail.com>
2025-01-29 11:28:35 +1100

    Correct a typo in error message

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -37,7 +37,28 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
-            raise TypeError("height must be an integer")
+            raise TypeError("hegiht must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        """Calculates area of the rectangle object"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """Calculates perimeter of the rectangle object"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__height + self.__width)
+
+    def __str__(self):
+        result = ""
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                result = result + "#"
+            if not i == self.__height - 1:
+                result = result + "\n"
+        return result
