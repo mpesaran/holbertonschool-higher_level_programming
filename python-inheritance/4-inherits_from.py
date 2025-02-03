0cee0779480f80e8ecd9fad6b145d03ac168dfb5
mpesaran <mahsa.pesaran@gmail.com>
2025-02-04 08:18:01 +1100

    Check the obj isnt an instance of the a_class

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,4 +7,13 @@ specified class
 
 def inherits_from(obj, a_class):
     """Returns true if object is inhereted of a_class otherwise false"""
-    return type(obj) is a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
+
+
+a = True
+if inherits_from(a, int):
+    print("{} inherited from class {}".format(a, int.__name__))
+if inherits_from(a, bool):
+    print("{} inherited from class {}".format(a, bool.__name__))
+if inherits_from(a, object):
+    print("{} inherited from class {}".format(a, object.__name__))
