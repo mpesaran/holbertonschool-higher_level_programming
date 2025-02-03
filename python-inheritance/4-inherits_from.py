90191d2b3e5f0996d8e0d7c50a0987c1010bb038
mpesaran <mahsa.pesaran@gmail.com>
2025-02-04 08:09:31 +1100

    Change the method to look for inherited class

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,4 +7,4 @@ specified class
 
 def inherits_from(obj, a_class):
     """Returns true if object is inhereted of a_class otherwise false"""
-    return issubclass(obj, a_class)
+    return type(obj) is a_class
