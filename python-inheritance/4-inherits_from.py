b0b595dd9cb2ca97cbd3330766288706e568c772
mpesaran <mahsa.pesaran@gmail.com>
2025-02-04 08:18:50 +1100

    delete extra lines to test

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -8,12 +8,3 @@ specified class
 def inherits_from(obj, a_class):
     """Returns true if object is inhereted of a_class otherwise false"""
     return isinstance(obj, a_class) and type(obj) is not a_class
-
-
-a = True
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
-if inherits_from(a, bool):
-    print("{} inherited from class {}".format(a, bool.__name__))
-if inherits_from(a, object):
-    print("{} inherited from class {}".format(a, object.__name__))
