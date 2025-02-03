f09c49a23337b4745678744ba54fa83390107242
mpesaran <mahsa.pesaran@gmail.com>
2025-02-04 10:24:44 +1100

    Add a new class rectangle inherits from BaseGeometry class

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -6,6 +6,7 @@ This module is to have a class which has an empty method
 
 class BaseGeometry:
     """This class has a method area which raises an exception"""
+
     def area(self):
         raise Exception("area() is not implemented")
 
---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module is to have a class which has an empty method
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """This class initiates a rectangle object"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
