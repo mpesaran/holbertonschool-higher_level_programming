a73c5f6375f00c94e7f573b38ba131d4e110ac2f
mpesaran <mahsa.pesaran@gmail.com>
2025-02-04 09:44:15 +1100

    Add a function to class to validate values

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module is to have a class which has an empty method
+"""
+
+
+class BaseGeometry:
+    """This class has a method area which raises an exception"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates if the value for name is integer and greter than 0"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value < 0:
+            raise ValueError("{} must be greater than 0".format(name))
