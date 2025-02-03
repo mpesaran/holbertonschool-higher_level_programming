f056826d5a27273b466b9ee12c44c8192ce23925
mpesaran <mahsa.pesaran@gmail.com>
2025-02-03 15:37:36 +1100

    Add a function to check if obj is instance of a class

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module is to have a function to chaeck if function is an instance of
+specified class or its subclasses
+"""
+
+
+def is_same_class(obj, a_class):
+    """Returns true if obje is instance of a_class otherwise false"""
+    return isinstance(obj, a_class)
