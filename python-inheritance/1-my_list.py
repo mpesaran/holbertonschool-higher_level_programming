4e3518d79499daec06a81afe59607c955a283464
mpesaran <mahsa.pesaran@gmail.com>
2025-02-03 14:32:22 +1100

    Correct a typo in class name

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,8 +4,9 @@ This module defines a new class which inherits list type
 """
 
 
-class Mylist(list):
+class MyList(list):
     """new class that is a child of list class"""
+
     def print_sorted(self):
         """prints sorted list"""
         print(sorted(self))
