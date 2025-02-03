017f59969f73ef9a04663a8099efb5d908a9d5ab
mpesaran <mahsa.pesaran@gmail.com>
2025-02-04 10:42:49 +1100

    Delete whitespaces

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -12,10 +12,10 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-        
+
     def area(self):
         """Calculates area of the rectangle"""
         return self.__width * self.__height
-    
+
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
