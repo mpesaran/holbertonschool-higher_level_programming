8f47ce0aa83c665d2b07f33fcc8af11242405b0d
mpesaran <mahsa.pesaran@gmail.com>
2025-02-04 14:43:02 +1100

    Add a new interface class and two child classes

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""This module is to have an abstract class"""
+from abc import ABC, abstractmethod
+import math
+
+
+class Shape(ABC):
+    """This is an abstract class"""
+    @abstractmethod
+    def area(self):
+        """This is an abstract method to calculate area"""
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        """This is an abstrat method to calculate perimeter"""
+        pass
+
+
+class Circle(Shape):
+    """This one is a child class of abstract class Animal"""
+    def __init__(self, radius):
+        super().__init__()
+        self.__radius = radius
+
+    def perimeter(self):
+        return 2 * self.__radius * math.pi
+
+    def area(self):
+        return (self.__radius ** 2) * math.pi
+
+
+class Rectangle(Shape):
+    """This one is a child class of abstract class Animal"""
+    def __init__(self, width, height):
+        super().__init__()
+        self.__width = width
+        self.__height = height
+
+    def perimeter(self):
+        return 2 * (self.__width + self.__height)
+
+    def area(self):
+        return self.__height * self.__width
+
+
+def shape_info(sh):
+    """A function to call perimeter and area methods regardless of
+    objects actual class
+    """
+    print(sh.area())
+    print(sh.perimeter())
