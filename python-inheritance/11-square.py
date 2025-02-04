8538d5715f91bc9e88f777989462b853e0f1843a
mpesaran <mahsa.pesaran@gmail.com>
2025-02-04 11:26:07 +1100

    Add a str function owned by square class

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module is to have a class which has an empty method
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """This class initiates a square object"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Calculates area of the rectangle"""
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
