abf28aad5c1923c4a18c8d7cfce4790ca5086050
mpesaran <mahsa.pesaran@gmail.com>
2025-02-05 09:57:52 +1100

    Add a class to extend list class

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""This module is to extend built-in classes to add
+or modify behavior"""
+from abc import ABC
+
+
+class VerboseList(list):
+    """This is an extended class for list type"""
+    def append(self, item):
+        print(f"Added [{item}] to the list.")
+        return super().append(item)
+
+    def extend(self, iterable):
+        print(f"Extended the list with [{len(iterable)}] items.")
+        return super().extend(iterable)
+
+    def pop(self, index=-1):
+        print(f"Popped [{self[index]}] from the list.")
+        return super().pop(index)
+
+    def remove(self, value):
+        print(f"Removed [{value}] from the list.")
+        return super().remove(value)
