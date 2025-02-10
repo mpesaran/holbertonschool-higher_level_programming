89562aa3c3c924ed417c3ebf4f7d3405e17f0aae
mpesaran <mahsa.pesaran@gmail.com>
2025-02-10 15:55:28 +1100

    Add a function to serialize an object

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/sur/bin/pyhton3
+"""This module is about convert to JSON string"""
+import json
+
+
+def to_json_string(my_obj):
+    """returns th eJSON representation of an object"""
+    return json.dumps(my_obj)
