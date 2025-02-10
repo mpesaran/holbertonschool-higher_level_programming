58121fe56d00ff7e1cef445588e635b993fe500c
mpesaran <mahsa.pesaran@gmail.com>
2025-02-10 15:19:23 +1100

    Omit new line at the end of the file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """This function will open a text file and read it and close it"""
     with open(filename, encoding='utf-8') as f:
         read_data = f.read()
-        print(read_data)
+        print(read_data, end="")
