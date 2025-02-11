8dfd668baed943389bc6ec84e2a542255226e882
mpesaran <mahsa.pesaran@gmail.com>
2025-02-11 16:38:21 +1100

    Delete extra appening to item inside loop

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -5,18 +5,28 @@ a Pascal triangle"""
 
 def pascal_triangle(n):
     """Create a Pascal trinagle with n rows"""
-    triangle = []
-    item = []
     if n <= 0:
         return []
     if n == 1:
-        triangle.append([1])
-
+        return [[1]]
+    triangle = [[1]]
     for i in range(1, n):
         item = [1]
         previous_item = triangle[i - 1]
-        for j in range(1, n):
-            item.append(item.append(previous_item[j - 1] + previous_item[j]))
+        for j in range(1, len(previous_item)):
+            item.append(previous_item[j - 1] + previous_item[j])
         item.append(1)
         triangle.append(item)
     return triangle
+
+
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
