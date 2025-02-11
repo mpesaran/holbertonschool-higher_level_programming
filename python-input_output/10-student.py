f1adebee5f7945c71b17245ddd642eed397448ee
mpesaran <mahsa.pesaran@gmail.com>
2025-02-11 14:44:37 +1100

    Change function to specify the attributes mentioned

---
+++ python-input_output/10-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module is about write a class called student"""
+
+
+class Student:
+    """this class has 3 public attributes and one public method"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """convert object to a dictionary"""
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        return self.__dict__
