92e16db2ce211f4f8a3a9221510304038dd6e472
mpesaran <mahsa.pesaran@gmail.com>
2025-02-20 09:49:18 +1100

    uppercase the S in printing format

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -10,7 +10,7 @@ def fetch_and_print_posts():
     """Fetches posts from jsonplaceholder and prints to the terminal."""
     url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
-    print(f"Status code: {response.status_code}")
+    print(f"Status Code: {response.status_code}")
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
