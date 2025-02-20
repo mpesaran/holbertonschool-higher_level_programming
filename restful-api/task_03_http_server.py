4af1511576fd6711afee958c48768a8a603e0b04
mpesaran <mahsa.pesaran@gmail.com>
2025-02-20 12:33:10 +1100

    Create a server and simple API using http server module

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Develop a simple API using Python's http.server module"""
+import http.server
+import socketserver
+import json
+
+
+class SimpleRequestHandler(http.server.BaseHTTPRequestHandler):
+    """This is a subclass of http.server.BaseHTTPRequestHandler"""
+    def do_GET(self):
+        """Handles GET requests with a text message"""
+        if self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode("utf-8"))
+        elif self.path == "/":
+            self.send_response(200, message="Hello, this is a simple API!")
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps({'status': 'OK'}).encode("utf-8"))
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps({
+                'error': 'Not Found',
+                'message': 'The requested endpoint does not exist'
+                }).encode('utf-8'))
+
+
+def start_server():
+    """Starts the HTTP server on port 8000"""
+    port = 8000
+    with socketserver.TCPServer(("", port), SimpleRequestHandler) as httpd:
+        print(f"Server on port {port}...")
+        httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    start_server()
