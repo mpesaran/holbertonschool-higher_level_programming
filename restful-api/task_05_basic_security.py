d1e5a1f729778021134b1f51ca056cf64628ca53
mpesaran <mahsa.pesaran@gmail.com>
2025-02-21 22:22:05 +1100

    Add data to the file

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -13,8 +13,8 @@ auth = HTTPBasicAuth()
 jwt = JWTManager(app)
 
 users = {
-    # "user1": {"username": "user1", "password": generate_password_hash("password"), "role": "user"},
-    # "admin1": {"username": "admin1", "password": generate_password_hash("password"), "role": "admin"}
+    "user1": {"username": "user1", "password": generate_password_hash("password"), "role": "user"},
+    "admin1": {"username": "admin1", "password": generate_password_hash("password"), "role": "admin"}
 }
 
 @auth.verify_password
