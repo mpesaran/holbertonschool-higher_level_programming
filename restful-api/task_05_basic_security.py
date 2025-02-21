01387ea2dd761127a94a9e17602532be4b101640
mpesaran <mahsa.pesaran@gmail.com>
2025-02-21 16:37:35 +1100

    Correct syntax in creating a token

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -4,8 +4,8 @@ from flask_httpauth import HTTPBasicAuth
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from werkzeug.security import generate_password_hash, check_password_hash
 users = {
-    # "user1": {"username": "user1", "password": generate_password_hash("password"), "role": "user"},
-    # "admin1": {"username": "admin1", "password": generate_password_hash("password"), "role": "admin"}
+    "user1": {"username": "user1", "password": generate_password_hash("password"), "role": "user"},
+    "admin1": {"username": "admin1", "password": generate_password_hash("password"), "role": "admin"}
 }
 
 app = Flask(__name__)
@@ -34,7 +34,7 @@ def login():
     username = data.get("username")
     password = data.get("password")
     if username in users and check_password_hash(users[username]["password"], password):
-        access_token = create_access_token({"username": username, "role": users[username]["role"]})
+        access_token = create_access_token(identity={"username": username, "role": users[username]["role"]})
         return jsonify(access_token=access_token)
     return jsonify({"message": "Invalid credentials"}), 401
 
