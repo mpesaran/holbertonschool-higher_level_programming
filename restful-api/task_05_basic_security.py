672fda08c1e3452d6bdaa88ccaf14b85e9e5cea7
mpesaran <mahsa.pesaran@gmail.com>
2025-02-21 16:37:51 +1100

    Correct syntax in creating a token

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -4,8 +4,8 @@ from flask_httpauth import HTTPBasicAuth
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from werkzeug.security import generate_password_hash, check_password_hash
 users = {
-    "user1": {"username": "user1", "password": generate_password_hash("password"), "role": "user"},
-    "admin1": {"username": "admin1", "password": generate_password_hash("password"), "role": "admin"}
+    # "user1": {"username": "user1", "password": generate_password_hash("password"), "role": "user"},
+    # "admin1": {"username": "admin1", "password": generate_password_hash("password"), "role": "admin"}
 }
 
 app = Flask(__name__)
