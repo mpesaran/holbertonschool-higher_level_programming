3d49b7c37a775587a1d57959b0bea57bb3c0eeee
mpesaran <mahsa.pesaran@gmail.com>
2025-03-03 16:31:55 +1100

    List shows title and genres from 2 tables

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- List all the shows n a table that have at least one genre linked
+SELECT tv_shows.title, tv_shows_genres.genre_id
+FROM tv_shows
+JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.show_id
+ORDER BY tv_shows.title, tv_shows_genres.genre_id;
