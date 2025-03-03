73ae183e730ece3bd6b82b138dc705ee308e834c
mpesaran <mahsa.pesaran@gmail.com>
2025-03-03 15:26:06 +1100

    Check if the user already exists on server

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- Creates a new user on server if not existed
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Creates a new user on server if not existed and give all previleges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
