1ab5d2fb1d26ab483c54267d7b2eb7e2ba05fd37
mpesaran <mahsa.pesaran@gmail.com>
2025-03-03 17:05:16 +1100

    Correct the script to group and join tables

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- List all genres from DB and display the number of shows linked to each genre
-SELECT tv_show_genres.name AS genre, COUNT(*) number_of_shows
-FROM tv_show_genres
-RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-GROUP BY tv_genres.name
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_shows_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.id
 ORDER BY number_of_shows DESC;
