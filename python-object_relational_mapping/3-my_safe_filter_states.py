5ae68274a2713219cf11166aac174922f810ecc4
mpesaran <mahsa.pesaran@gmail.com>
2025-03-04 08:39:19 +1100

    Add a script to prevent sql injection by user input

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+This script uses the MySQLdb module to connect to a MySQL database
+and lists values equal to argument given by user from 'states' table in
+ the specified database, if start with N. The results are sorted in
+ ascending order by the state ID and displayed as tuples.
+"""
+
+import sys
+import MySQLdb
+
+
+def filter_states():
+    """
+    This function will list values in states table matches
+    argument passed by user considering SQL injection
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        password=password,
+        db=database,
+        charset="utf8"
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT id, name\
+                   FROM states\
+                   WHERE BINARY name = %s\
+                   ORDER BY id", (state_name,)
+                   )
+    results = cursor.fetchall()
+    for state in results:
+        print(state)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    filter_states()
