ee0ff268116eda53d135bc2f3a9558236dc7c244
mpesaran <mahsa.pesaran@gmail.com>
2025-03-03 12:52:02 +1100

    Correct the typo in row values

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,12 @@
--- Create a new table in database and add multiple rows at the same time
+-- Create a new table in database
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
+-- Insert multiple rows at the same time
 INSERT INTO second_table (id, name, score) VALUES
-(1, 'Jhon', 10),
+(1, 'John', 10),
 (2, 'Alex', 3),
 (3, 'Bob', 14),
 (4, 'George', 8);
