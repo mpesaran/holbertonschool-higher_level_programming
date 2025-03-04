29ef9dab6c39b9a989f4a8021cf0ed780433ce47
mpesaran <mahsa.pesaran@gmail.com>
2025-03-05 10:26:51 +1100

    Add a script to select just records that contain a in name

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Fetches the state objects that contain the letter
+'a' from database usin sqlAlchemy
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import State, Base
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]
+    ), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    result = session.query(State).filter(State.name.ilike('%a%')).order_by(State.id)
+    for state in result:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
