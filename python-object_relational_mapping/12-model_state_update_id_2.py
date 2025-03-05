5ceabc62555d9b138744f03cc6e029bccd1aefed
mpesaran <mahsa.pesaran@gmail.com>
2025-03-06 10:15:17 +1100

    Update a record in database

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Updates a record's name in database 
+"""
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]
+    ))
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+
+    state_to_update = session.query(State).filter_by(id=2).first()
+
+    if state_to_update:
+        state_to_update.name = "New Mexico"
+    session.commit()
